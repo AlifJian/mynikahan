741d2b52f66d53846826c0033176da44608732cf
Alif Jian <alifjian834@gmail.com>
2026-02-14 22:36:17 +0700

    feat: update template 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${serif.variable} ${sans.variable}`}>
         {children}
       </body>
     </html>
